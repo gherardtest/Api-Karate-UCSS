904f6b8a0fdb0662c9a0b47db0f06e067c5814b5
gherardtest <gherardtest@gmail.com>
2023-01-28 22:36:30 -0500

    Karate Report

--- src/test/java/users/ManagementTest.java
+++ src/test/java/users/ManagementTest.java
@@ -1,7 +1,9 @@
 package users;
 
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import cucumber.api.CucumberOptions;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
@@ -12,19 +14,23 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@CucumberOptions(
+        features = "src/test/java/users",
+format = {"json:target/cucumber.json"})
 public class ManagementTest {
-
     @Test
     void testParallel(){
-      Results results =  Runner.path("classpath:users").outputCucumberJson(true).tags("~@ignore").parallel(4);
-      generateReport(results.getReportDir());
+        System.out.println("getClass:" + getClass());
+        Results results = Runner.parallel(getClass(),1,"target/karate-report");
+              //Runner.path("classpath:users").outputCucumberJson(true).tags("~@ignore").parallel(4);
+        generateReport(results.getReportDir());
     }
 
     public static void generateReport(String karateOutputPath){
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
         List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("build"), "Api-Karate-UCSS");
+        Configuration config = new Configuration(new File("target"), "Api-Karate-UCSS");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
