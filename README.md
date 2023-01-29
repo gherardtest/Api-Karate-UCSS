bfd16cbebae33faedfa65ea6c9d5560578a375a8
gherardtest <gherardtest@gmail.com>
2023-01-29 00:00:44 -0500

    cucumber

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# basic_karate
+# Api-Karate-UCSS
 
 Proyecto basico de karate utilizando el API de https://reqres.in/ usando diferentes metodos POST,GET,PUT,DELETE. Implementando diferentes funciones de karate.
--- src/test/java/users/ManagementTest.java
+++ src/test/java/users/ManagementTest.java
@@ -13,22 +13,22 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-@KarateOptions(features = {"classpath:users"})
+import static junit.framework.TestCase.assertTrue;
+@KarateOptions(tags = {"~@ignore"})
 public class ManagementTest {
     @Test
-    void testParallel(){
-        System.out.println("getClass:" + getClass());
-        Results results = Runner.parallel(getClass(),1,"target/surefire-reports");
-              //Runner.path("classpath:users").outputCucumberJson(true).tags("~@ignore").parallel(4);
-        generateReport(results.getReportDir());
-    }
-
-    public static void generateReport(String karateOutputPath){
-        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
-        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+    public void testParallel() {
+        System.setProperty("mock.env", "karateTesting"); // ensure reset if other tests (e.g. mock) had set env in CI
+        Results results = Runner.parallel(getClass(), 5);
+        ManagementTest.generateReport(results.getReportDir());
+        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);}
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("target"), "Api-Karate-UCSS");
+        Configuration config = new Configuration(new File("target"), "karateTesting");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
 }
+
